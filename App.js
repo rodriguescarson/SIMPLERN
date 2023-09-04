1bd8a3a03512fec67eb475519507db0af0d5c003
Carson <rodriguescarson@hotmail.com>
2023-09-04 23:05:50 +0530

    style: Enhance login screen UI
    - Increased font size and added bold styling to the title
    - Rounded the corners of input fields for a rounded look
    - Adjusted colors for a more visually appealing design
    - Improved layout and spacing for better aesthetics

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Hardcoded username and password for demo purposes
+    const hardcodedUsername = 'user';
+    const hardcodedPassword = 'password';
+
+    if (username === hardcodedUsername && password === hardcodedPassword) {
+      Alert.alert('Login successful');
+    } else {
+      Alert.alert('Login failed');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Button
+        title="Login"
+        onPress={handleLogin}
+        color="#007BFF" // Custom button color
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5', // Background color
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold', // Bold text
+    marginBottom: 20,
+    color: '#333', // Text color
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10, // Rounded corners
+    marginBottom: 20,
+    paddingLeft: 15, // Left padding
+    fontSize: 16, // Font size
+    backgroundColor: '#fff', // Input background color
   },
 });
+
+export default App;
